5514a82a275b80e23953442776da2f7511a9618f
Jhosett <darwinjhosettbermudezromero@gmail.com>
2023-12-12 18:55:43 -0500

    Ejercicio terminado

--- practicaExamenDes2B/src/App.vue
+++ practicaExamenDes2B/src/App.vue
@@ -5,11 +5,16 @@ let contH = ref(20)
 let contD = ref(3)
 
 function sumarCorazon() {
+  if (contH.value == 20) {
+    contH.value++
+    contD.value = 3
+  }
+}
+
+function sumarDislike() {
   if (contD.value == 3) {
-    contH.value++,
-  }else{
-    contH.value++,
-    contD--,
+    contD.value++
+    contH.value = 20
   }
 }
 
@@ -61,16 +66,16 @@ function sumarCorazon() {
 
       <div class="flex mt-2 mb-4">
         <div class="flex">
-          <svg @dblclick="sumarCorazon" @click="restarCorazon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <svg @click="sumarCorazon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
           </svg>
-          <p class="ml-1">20</p>
+          <p class="ml-1">{{ contH }}</p>
         </div>
         <div class="flex ml-6">
-          <svg @dblclick="sumarDislike" @click="restarDislike" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <svg @click="sumarDislike" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
             <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384" />
           </svg>
-          <p class="ml-1">3</p>
+          <p class="ml-1">{{ contD }}</p>
         </div>
       </div>
 
